dbcdbbb799eda88000dc8ce4f046d139ca6a94bf
Marco Antonio Tancredo <marcotancredo@gmail.com>
2021-03-09 23:29:08 -0300

    Using Roles to permissions

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -47,15 +47,16 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-            .antMatchers(HttpMethod.GET, "/topicos").permitAll()
-            .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
-            .antMatchers(HttpMethod.POST, "/auth").permitAll()
-            .antMatchers(HttpMethod.GET, "/actuator").permitAll()
-            .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-            .anyRequest().authenticated()
-            .and().csrf().disable()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class);
+                .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .antMatchers(HttpMethod.POST, "/auth").permitAll()
+                .antMatchers(HttpMethod.GET, "/actuator").permitAll()
+                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/topicos/*").hasRole("MODERADOR")
+                .anyRequest().authenticated()
+                .and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class);
     }
 
     //Configurações de recuros estaticos(js, css, imagens, etc.)
